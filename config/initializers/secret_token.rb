c0d02192c4a662f94c69b946b6428fb9c14a6467
Vysakh Sreenivasan <diplomatv@gmail.com>
2013-08-26 23:02:31 +0530

    Initial commit - create a rails starter app
    Includes devise, cancan, bootstrap, active admin, few others

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Custom::Application.config.secret_key_base = '27374bccd265e3532d189870fd61a4d32c50a889d064a2e42591641d0b20c1c56765065b3c6d93149875724df20dddd188fb623f4539110dd25f74f41056de9f'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Custom::Application.routes.draw do
+  root :to => "home#index"
+  devise_for :users, :controllers => {:registrations => "registrations"}
+  resources :users
+end
