b7e97daa8d30335d2c1727a197040a28c5964eb5
Abhirama sundram <abhiramasundram2007@gmail.com>
2013-09-05 15:11:52 +0530

    dashboard path changed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,16 +7,18 @@ class RegistrationsController < Devise::RegistrationsController
   # my custom fields are :name, :heard_how
  
 
+  
   def update_sanitized_params
     devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation, :performer_attributes => [ :first_name, :avatar, :photo_id ] )}
   end
 
   def after_sign_up_path_for(resource)
-    new_performer_path
+    edit_user_registration_path
   end
   
   def after_update_path_for(resource)
-    performers_path
+    performers_performer_dashboard_path
   end
+
 end
                                                                                                                      
